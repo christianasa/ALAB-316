f3790b0e0a0b2b11a79e6ebb9cfabc41090a4c03
Christiana <Christiana@Christianas-MacBook-Air.local>
2025-12-16 11:58:29 -0700

    Start of Part 1

---
+++ Script/script.js
@@ -0,0 +1,10 @@
+// Part 1:
+
+const mainEl = document.querySelector('main');
+
+
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+// Set the content to <h1>DOM Manipulation</h1>
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr');
