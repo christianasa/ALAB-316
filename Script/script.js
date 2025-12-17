08db2c02197cbc08d41ae1ce1452b90de6b3c45f
Christiana <Christiana@Christianas-MacBook-Air.local>
2025-12-16 21:16:52 -0700

    part 3

--- Script/script.js
+++ Script/script.js
@@ -1,13 +1,13 @@
 // Part 1: --------------------------------------------------------------------------------------------------------------------------
-
-const mainEl = document.querySelector('main');
-
+let mainEl = document.querySelector("main");
 
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 
 mainEl.classList.add('flex-ctr');
+
+//const mainEl = document.querySelector('main')
 // Part 2: -----------------------------------------------------------------------------------------------------------------------
 
 // Cache
@@ -23,3 +23,18 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.classList.add('flex-around');
 
 // Part 3: ------------------------------------------------------------------------------------------------------------------
+
+// 1.   Iterate over the entire menuLinks array and for each "link" object:
+for (let link of menuLinks) {
+  // 2.   Create an <a> element.
+  let anchor = document.createElement("a");
+
+  // 3.   On the new element, add an href attribute with its value set to the href property of the "link" object.
+  anchor.setAttribute("href", link.href);
+
+  // 4.   Set the new element's content to the value of the text property of the "link" object.
+  anchor.textContent = link.text;
+
+  // 5.   Append the new element to the topMenuEl element.
+  topMenuEl.appendChild(anchor);
+}
